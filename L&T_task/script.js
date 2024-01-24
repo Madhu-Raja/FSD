530de52356d9ae9be4c5e3626f0bf70c55272cb9
Madhumitha Raja <89344792+Madhu-Raja@users.noreply.github.com>
2024-01-24 14:25:21 +0530

    Add files via upload

---
+++ L&T_task/script.js
@@ -0,0 +1,18 @@
+// script.js
+function register() {
+    // Fetch user input values
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+  
+    // TODO: Perform validation on the input fields
+  
+    // For demonstration purposes, log the data to the console
+    console.log('Name:', name);
+    console.log('Email:', email);
+    console.log('Password:', password);
+  
+    // TODO: Send registration data to the server for further processing
+    // Include logic on the server to send a verification email with an activation link
+  }
+  
